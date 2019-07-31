f5420fc3585f0bd520b4959757fbc9d46aadece8
thiagodma <thiagodma52@gmail.com>
2019-07-31 12:21:35 -0300

    Foram feitas modificações para que o main não conflita com a biblioteca

--- main.py
+++ main.py
@@ -1,47 +1,40 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from scipy.cluster import hierarchy
-from datetime import datetime
 import pandas as pd
 import cluster_normas_funcoes as cnf
+import time
 import matplotlib.pyplot as plt
 
-# Controle de tempo
-ti = datetime.now()
-print('Iniciado as: ' + str(ti) + '\n') 
 
-stop_words = [cnf.limpa_utf8(w) for w in cnf.stop_words]
+#stopwords definidas
+stop_words = cnf.define_stop_words()
 
 # Input de arquivos
-resolucoes, nome_arquivos = cnf.importa_normas()
+resolucoes_tratadas, resolucoes, nome_arquivos = cnf.importa_normas(stop_words)
 
 #Faz o stemming
 resolucoes_stem = cnf.stem(resolucoes)
 
-#Checando o tempo para importar e tratar os textos
-timport = datetime.now()
-print('Tempo para importar e tratar textos: ', timport - ti)
-
-#Vetorizando
+#Vetorizando e aplicando o tfidf
 vec = CountVectorizer()
 bag_palavras = vec.fit_transform(resolucoes_stem)
 feature_names = vec.get_feature_names()
 base_tfidf = TfidfTransformer().fit_transform(bag_palavras)
-
-#Tornando a matriz densa
 base_tfidf = base_tfidf.todense()
 
 #Reduzindo a dimensionalidade
 base_tfidf_reduced = cnf.SVD(600, base_tfidf)
 
 #Clustering
+print('Começou a clusterização.')
+t = time.time()
 clusters_por_cosseno = hierarchy.linkage(base_tfidf_reduced,"average", metric="cosine") #pode testar metric="euclidean" também
 #plt.figure()
 #dn = hierarchy.dendrogram(clusters_por_cosseno)
-
-# Separa a que Cluster pertence cada texto, pela ordem na lista de textos,
-# dado o parâmetro de limite de dissimilaridade threshold
 limite_dissimilaridade = 0.85
 id_clusters = hierarchy.fcluster(clusters_por_cosseno, limite_dissimilaridade, criterion="distance")
+elpsd = time.time() - t
+print('Tempo para fazer a clusterização: ' + str(elpsd) + '\n')
 
 analise = cnf.analisa_clusters(base_tfidf_reduced, id_clusters)
 
@@ -57,6 +50,3 @@ print('Foram encontradas ' + str(max(Z['cluster_id'])) + ' clusters\n')
 #Exporta as tabelas
 Z.to_csv('cluster_normas_cosseno.csv', sep='|', 
                     index=False, encoding='utf-8')
-
-#Printa o tempo total de execução do script
-print('Tempo total: ',datetime.now() - ti)
