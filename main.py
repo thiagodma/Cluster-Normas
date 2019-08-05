7182cd0fed0811d69ba75a4d5635ae560e0e32a9
thiagodma <thiagodma52@gmail.com>
2019-08-05 12:32:40 -0300

    Main atualizado para não conflitar com a biblioteca

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ stop_words = cnf.define_stop_words()
 # Input de arquivos
 resolucoes_tratadas, resolucoes, nome_arquivos = cnf.importa_normas(stop_words)
 
+arquivos_fora_padrao=[]
+
+for resolucao,nome_arquivo in zip(resolucoes_tratadas,nome_arquivos):
+    if resolucao == 'norma fora de padrão':
+        arquivos_fora_padrao.append(nome_arquivo)
+
 #Faz o stemming
-resolucoes_stem = cnf.stem(resolucoes)
+resolucoes_stem = cnf.stem(resolucoes_tratadas)
 
 #Vetorizando e aplicando o tfidf
 vec = CountVectorizer()
@@ -31,7 +37,7 @@ t = time.time()
 clusters_por_cosseno = hierarchy.linkage(base_tfidf_reduced,"average", metric="cosine") #pode testar metric="euclidean" também
 #plt.figure()
 #dn = hierarchy.dendrogram(clusters_por_cosseno)
-limite_dissimilaridade = 0.85
+limite_dissimilaridade = 0.9
 id_clusters = hierarchy.fcluster(clusters_por_cosseno, limite_dissimilaridade, criterion="distance")
 elpsd = time.time() - t
 print('Tempo para fazer a clusterização: ' + str(elpsd) + '\n')
@@ -41,9 +47,10 @@ analise = cnf.analisa_clusters(base_tfidf_reduced, id_clusters)
 #Colocando em dataframes
 X = pd.DataFrame(id_clusters,columns=['cluster_id'])
 Y = pd.DataFrame(nome_arquivos,columns=['norma'])
-
+W = pd.DataFrame(list(range(len(resolucoes_tratadas))), columns=['codigo_norma'])
 # Matriz clusterização
 Z = X.join(Y)
+Z = Z.join(W)
 
 print('Foram encontradas ' + str(max(Z['cluster_id'])) + ' clusters\n')
 
