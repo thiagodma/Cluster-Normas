3363320d92ab338dcd05b230d39f1f9f3acad041
thiagodma <thiagodma52@gmail.com>
2019-08-05 12:32:13 -0300

    nada importante

--- teste.py
+++ teste.py
@@ -1,4 +1,14 @@
 
 import cluster_normas_funcoes as cnf
+import re
+cnf.trata_textos(s,stop_words)
 
-cnf.corrige_nome_arquivos()
+
+#re.findall(r'\nart. \d',texto)
+
+#r'\nArt. 1(.*)\nArt. 5'
+
+count = 0
+for resolucao in resolucoes_tratadas:
+    if resolucao == 'norma fora de padrão':
+        count = count + 1
