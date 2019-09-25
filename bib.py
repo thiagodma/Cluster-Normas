b34b0a58ea79dfa9eb5fcbff5a23974d3bd74da0
Thiago Dantas <thiagodma52@gmail.com>
2019-09-25 12:24:27 -0300

    Adicionado método para normalizar os nomes dos arquivos

--- bib.py
+++ bib.py
@@ -15,10 +15,12 @@ class Data():
         self.macrotemas = []
         self.nomes_normas = []
         self.ementas = []
+        self.tipos_norma = {'PRT':'Portaria', 'RDC':'RDC', 'RES':'RE', 'RE':'RE',
+                       'IN':'IN', 'INC':'INC', 'PRTC':'PRTC'}
 
     def import_data(self):
 
-        path = 'Arquivos_DOCX_atual_31_julho_2019/'
+        path = 'Normas_novas_23set (atual)/'
         aux = [None for i in range(len(self.tabela['Citadora']))]
         self.tabela['isFile'] = aux
         for i in range(len(self.tabela['Citadora'])):
@@ -54,3 +56,31 @@ class Data():
         data_csv = pd.DataFrame(list(zip(self.nomes_normas,self.macrotemas,self.ementas,self.textos)),
         columns=['nomes_normas','macrotemas','ementas','textos'])
         data_csv.to_csv('Data.csv',sep='|',index=False,encoding='utf-8')
+
+    def normalize_file_names(self):
+
+        path = 'Normas_novas_23set (atual)'
+        files = []
+        for r, d, f in os.walk(path):
+            for file in f:
+                files.append(file)
+
+        os.chdir(path)
+        for filename in files:
+            raw_file = filename
+            # Procura o ultimo digito do nome do arquivo
+            find_digits = re.match('.+([0-9])[^0-9]*$', filename)
+            # Posicao do ultimo digito encontrada
+            pos_last_digit = find_digits.start(1)
+            # Corta o resto alem do ultimo di­gito
+            filename = filename[:pos_last_digit + 1]
+            # Separa no split por underline
+            filename = filename.split('_')
+            # Monta nome_arq em norma_citadora no padrao desejado tipo_norma numero_norma/AAAA
+            # JÃ¡ Ã© aplicado tambem o dicionario tipos_norma ao nome_arq[0], normalizando
+            # assim o tipo da norma
+            numeration = filename[-2].strip()
+            # Adicionando o '0' Ã s normas que sao numeral unico, como IN 1/2014
+            if len(numeration) == 1: numeration = '0' + numeration
+            out = self.tipos_norma[filename[0].strip()] + ' ' + numeration + '_' + filename[-1]
+            os.rename(raw_file,out+'.docx')
