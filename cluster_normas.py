5bfe13b7760d47773f33465c17d1bf1c4d0468cf
Thiago Dantas <thiagodma52@gmail.com>
2019-10-21 10:05:51 -0300

    adicionei as palavras mais repetidas de cada cluster no metodo dos csvs

--- cluster_normas.py
+++ cluster_normas.py
@@ -22,13 +22,23 @@ class ClusterNormas(ClassicClustering):
 
     def generate_csvs(self, cluster_n_textos, id_clusters):
 
-        textos_por_cluster = pd.DataFrame(zip(list(self.df.nomes_normas),list(id_clusters),self.textos,self.textos_tratados),
-                                          columns=['normas','cluster_id','textos','textos_tratados'])
+        textos_por_cluster = pd.DataFrame(zip(list(self.df.nomes_normas),
+                             list(id_clusters),self.textos,self.textos_tratados),
+                             columns=['normas','cluster_id','textos','textos_tratados'])
+
+        word_count = [self.freq_words_cluster(cluster,textos_por_cluster,5) for cluster in cluster_n_textos.cluster_id]
+
+        cluster_n_textos['palavras_importantes'] = word_count
+        info_cluster = cluster_n_textos
+
+        info_cluster.to_csv('info_cluster.csv', sep='|',index=False, encoding='utf-8')
+        textos_por_cluster.to_csv('textos_por_cluster.csv', sep='|',index=False, encoding='utf-8')
 
         return textos_por_cluster
 
-    def freq_words_cluster(self, artigos_tratados,qtd:int):
+    def freq_words_cluster(self, cluster_id:int, textos_por_cluster:df, qtd:int):
 
+        artigos_tratados = list((textos_por_cluster[textos_por_cluster.cluster_id==cluster_id]).textos_tratados)
         all_words = ''
         for i in range(len(artigos_tratados)):
             all_words = all_words + artigos_tratados[i] + ' '
@@ -36,4 +46,6 @@ class ClusterNormas(ClassicClustering):
         Counter = Counter(all_words)
         most_occur = Counter.most_common(qtd)
 
-        return most_occur
+        out = [item [0] for item in most_occur]
+
+        return ' '.join(out)
