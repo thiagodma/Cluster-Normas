709c46ea2a247b67724b9c58dde2dcc2861c5002
Thiago Dantas <thiagodma52@gmail.com>
2019-10-22 11:16:38 -0300

    adicionei uma função de busca

--- cluster_normas.py
+++ cluster_normas.py
@@ -26,7 +26,7 @@ class ClusterNormas(ClassicClustering):
                              list(id_clusters),self.textos,self.textos_tratados),
                              columns=['normas','cluster_id','textos','textos_tratados'])
 
-        word_count = [self.freq_words_cluster(cluster,textos_por_cluster,5) for cluster in cluster_n_textos.cluster_id]
+        word_count = [self.freq_words_cluster(cluster,textos_por_cluster,10) for cluster in cluster_n_textos.cluster_id]
 
         cluster_n_textos['palavras_importantes'] = word_count
         info_cluster = cluster_n_textos
@@ -47,7 +47,50 @@ class ClusterNormas(ClassicClustering):
 
         counter = Counter(all_words)
         most_occur = counter.most_common(qtd)
-        import pdb; pdb.set_trace()
         out = [item[0] for item in most_occur]
 
         return ' '.join(out)
+
+    def palavras2normas(self,palavras:list):
+
+        info_cluster = pd.read_csv('info_cluster.csv',sep='|',encoding='utf-8')
+        textos_por_cluster = pd.read_csv('textos_por_cluster.csv',sep='|',encoding='utf-8')
+
+        #aqui eu vejo quantas vezes as palavras dadas aparecem em cada cluster
+        count = np.zeros(info_cluster.shape[0])
+        for i in range(info_cluster.shape[0]):
+            keywords = re.split(' ',info_cluster.palavras_importantes.iloc[i])
+            for palavra in palavras:
+                count[i] = count[i] + (palavra in keywords)
+
+        #pego so as cluster que dão match com as palavras solicitadas
+        idxs = np.where(count == len(palavras))[0]
+
+        if len(idxs) == 0:
+            print('Deu ruim!')
+            return
+
+        all_count = []
+        all_normas = []
+        for idx in idxs:
+            aux = textos_por_cluster[textos_por_cluster.cluster_id == idx+1]
+            normas = []
+            #o contador agora é para cada norma
+            count = np.zeros(len(np.unique(aux.normas)))
+            #iterando nos artigos dentro de uma cluster
+            #import pdb; pdb.set_trace()
+            for i in range(aux.shape[0]):
+                #se essa norma ainda não apareceu
+                #import pdb; pdb.set_trace()
+                if aux.normas.iloc[i] not in normas: normas.append(aux.normas.iloc[i])
+                #conto o número de vezes que as palavras dadas aparecem na norma sendo analisada
+                for palavra in palavras:
+                    count[len(normas)-1] += (palavra in aux.textos_tratados.iloc[i])
+            all_count.extend(list(count))
+            all_normas.extend(normas)
+
+        sorted_idxs = np.argsort(all_count)
+        #esse array ta ordenado em ordem crescente
+        sorted_normas = [all_normas[idx] for idx in sorted_idxs]
+        #mas eu quero em ordem decrescente
+        return list(reversed(sorted_normas))
